6ad91e7c871cc5da9ca51d32a298dbaefc3e0252
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-06-09 02:21:54 +0000

    チャットルーム構築エラー修正

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,15 +1,15 @@
 class Public::ChatsController < ApplicationController
   
   def create
-    current_customer_chats = ChatRoom.where(customer_id: current_customer.id).map(&:chat)
-    chat = ChatRoom.where(chat: current_customer_chats, customer_id: params[:customer_id]).map.first
+    current_customer_chats = ChatRoom.where(customer_id: current_customer.id).map(&:chat_id)
+    chat = ChatRoom.where(chat_id: current_customer_chats, customer_id: params[:customer_id]).map.first
     
     if chat.blank?
       chat = Chat.create
       ChatRoom.create(chat_id: chat.id, customer_id: current_customer.id)
       ChatRoom.create(chat_id: chat.id, customer_id: params[:customer_id])
     end
-    redirect_to chats_path(chat)
+    redirect_to chat_path(chat.id)
   end
 
 
