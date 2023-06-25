d3a5e71f58ee1335bb42ae3a31263cb598ab0309
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-06-25 09:24:09 +0000

    新規会員登録bootstrap

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,26 +3,36 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
   has_many :chat_rooms
   has_many :chats, through: :chat_room
   has_many :friend_requests
   has_many :chat_messages
   has_many :seats
-  
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :school_year, presence: true
+  validates :faculty, presence: true
+  validates :introduction, presence: true
+
   def get_profile_image(width,height)
-    unless profile_image.attached? 
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   def full_name
     last_name + " " + first_name
   end
-  
+
   def full_name_kana
     last_name_kana + " " + first_name_kana
   end
