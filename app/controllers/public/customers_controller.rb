a477a7141cc33dd86ad8becd866dab31ba0528e1
EC2 Default User <ec2-user@ip-172-31-40-117.ap-northeast-1.compute.internal>
2023-08-31 02:31:43 +0000

    route並び替えテスト

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,7 +6,7 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     if @customer.anonymity == false && @customer.is_deleted == false
       render "show"
-    elsif @customer == current_customer.i
+    elsif @customer == current_customer
       render "show"
     else
       redirect_to root_path
